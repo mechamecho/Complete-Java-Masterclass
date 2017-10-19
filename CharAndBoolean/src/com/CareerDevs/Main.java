9f44a765bbe59315c7d2357f7b2b4940a3e56096
Livepurposefully <nafissa_92@hotmail.com>
2017-10-18 23:35:29 -0400

    Finish video about char and boolean in Java

---
+++ CharAndBoolean/src/com/CareerDevs/Main.java
@@ -0,0 +1,12 @@
+package com.CareerDevs;
+
+public class Main {
+
+    public static void main(String[] args) {
+//	char myChar='\u00A9';
+//        System.out.println("Unicode output was: "+ myChar);
+//        boolean myBoolean=false;
+        char registeredMark='\u00AE';
+        System.out.println("Registered Symbol is"+ registeredMark);
+    }
+}
