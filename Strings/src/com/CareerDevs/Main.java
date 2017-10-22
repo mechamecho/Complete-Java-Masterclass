e20b87ea0fd139d8f6ca5bb7c9f91218a82789a3
Livepurposefully <nafissa_92@hotmail.com>
2017-10-22 00:22:48 -0400

    Finish Strings lecture

---
+++ Strings/src/com/CareerDevs/Main.java
@@ -0,0 +1,20 @@
+package com.CareerDevs;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String myString="This is a string";
+        System.out.println("myString is equal to "+myString);
+        myString=myString+" and this is more.";
+        System.out.println("myString is equal to "+myString);
+        myString=myString+"\u00A9 2015";
+        System.out.println("myString is equal to "+myString);
+        String numberString="250.55";
+        numberString=numberString+"49.95";
+        System.out.println("The result is "+numberString);
+        String myLastString= "10";
+        int myInt=50;
+        myLastString=myLastString+myInt;
+        System.out.println("myLastString is equal to "+myLastString);
+    }
+}
